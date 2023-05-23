ed525dbfa50a2a651a2d86688e08ff5845565bd7
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-05-22 22:53:46 -0300

    feat: update bellman ford algorithm

--- Bellman-Ford/bellman_ford.cpp
+++ Bellman-Ford/bellman_ford.cpp
@@ -18,13 +18,11 @@ using pq = priority_queue<PairInt, VectorPairInt, greater<PairInt>>;
 const int INF = numeric_limits<int>::max();
 
 class Graph {
-    private:
+    public:
         int V;
         vector<int> path;
         vector <int> distance;
         vector<VectorPairInt> adjacentList;
-
-    public:
         Graph(int V) : V{V}, path(V), distance(V, INF), adjacentList(V) {}
 
         bool BellmanFord(int initial_vertex = 0) {
@@ -107,8 +105,9 @@ int main(int argc, char *argv[]) {
 
     int initial_vertex;
     bool hasNegativeCycle = false;
+    bool print = false;
     string input_file = "", output_file = "";
-    handle_args(argc, argv, input_file, output_file, initial_vertex);
+    handle_args(argc, argv, input_file, output_file, initial_vertex, print);
 
     ifstream fin(input_file);
     ofstream fout(output_file);
@@ -130,9 +129,26 @@ int main(int argc, char *argv[]) {
     if(initial_vertex < 1 || initial_vertex > V) initial_vertex = 1;
     hasNegativeCycle = G.BellmanFord(initial_vertex-1);
 
-    G.printBestPath();
-    G.printDistance();
+    if(print) {
+        G.printBestPath();
+        G.printDistance();
+    }
 
+    if(fout.is_open()){
+        fout << "Best path: " << endl;
+        for (int i = 0; i < V; i++) {
+            fout << i+1 << ": " << G.path[i]+1 << "\n";
+        }
+
+        fout << endl << endl;
+
+        fout << "Cost to get to each vertex: " << endl;
+        for (int i = 0; i < V; i++) {
+            string distance_str = G.distance[i] != INF ? to_string(G.distance[i]) : "Infinite";
+            fout << i+1 << " " <<  distance_str << endl;
+        }
+    }
+    
     if(hasNegativeCycle) cout << endl << "This graph contains a negative cycle" << endl;
 
     //G.printGraph();
--- Dijkstra/handleArgs.h
+++ Dijkstra/handleArgs.h
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void handle_args(int argc, char *argv[], string& input_file, string& output_file, int& s, bool& print) {
+void handle_args(int argc, char *argv[], string& input_file, string& output_file, int& s) {
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
             if (string(argv[i]) == "-f") {
@@ -16,8 +16,7 @@ void handle_args(int argc, char *argv[], string& input_file, string& output_file
                 }
             } else if (string(argv[i]) == "-o") {
                 if (i + 1 < argc) {
-                    output_file = "./outputs/" + string(+argv[i+1]);
-                    // add ./outputs/ to the output file name
+                    output_file = string(argv[i+1]);
                     i++;
                 } else {
                     cerr << "Error: output file name not provided after -o option" << endl;
@@ -31,15 +30,12 @@ void handle_args(int argc, char *argv[], string& input_file, string& output_file
                     cerr << "Error: vertex number not provided after -i option" << endl;
                     exit(1);
                 }
-            } else if (string(argv[i]) == "-s") {
-                print = true;
-            }
-            else if (string(argv[i]) == "-h") {
+            } else if (string(argv[i]) == "-h") {
                 cout << "Usage: " << argv[0] << " [-f input_file] [-o output_file] [-i starting_vertex] [-s]\n";
                 cout << "-f input_file: Specifies the input file name.\n";
                 cout << "-o output_file: Specifies the output file name.\n";
                 cout << "-i starting_vertex: Specifies the starting vertex number for Prim's algorithm.\n";
-                cout << "-s: Prints the graph and its ve.\n";
+                cout << "-s: Prints the minimum spanning tree in ascending order.\n";
                 cout << "-h: Prints this help message.\n";
                 exit(0);
             } else {
